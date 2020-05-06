50c244d57b51b4bec2fc3f017db8fc3e8aeb0f1c
AsecC-eror404 <64872316+AsecC-eror404@users.noreply.github.com>
2020-05-06 07:18:09 +0700

    Delete setup.sh

--- setup.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Oz='all ';Iz='te &';Mz='e';Hz='upda';Jz='& ap';az='yagm';Kz='t up';Ez='... ';lz='p 3';Pz='pyth';Wz=' ins';Az='echo';Tz='ests';Bz=' "In';Nz='inst';Yz=' yag';kz='slee';cz='pkg ';Lz='grad';Fz='"';Uz='est';iz=' gol';hz='thon';jz='d.py';Sz='requ';Xz='tall';bz='ail';ez=' "DO';gz=' "py';Cz='stal';Dz='ling';dz='git';Vz='pip2';Gz='apt ';Qz='on';Rz='pip ';fz='NE"';Zz='mail';
-eval "$Az$z$Az$Bz$Cz$Dz$Ez$Fz$z$Az$z$Gz$Hz$Iz$Jz$Kz$Lz$Mz$z$Gz$Nz$Oz$Pz$Qz$z$Rz$Nz$Oz$Sz$Tz$z$Rz$Nz$Oz$Sz$Uz$z$Vz$Wz$Xz$Yz$Zz$z$Rz$Nz$Oz$az$bz$z$cz$Nz$Oz$dz$z$Az$z$Az$ez$fz$z$Az$gz$hz$iz$jz$Fz$z$kz$lz"
